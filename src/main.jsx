3731e28b796843f3ec8137343cd3e8a83a59c377
Roger Norling <roger.norling.se@gmail.com>
2018-08-02 00:45:14 +0200

    Show a simple toggle item
    Uses the bow as the first simple item that can toggle state.
    Also adds the styled-components and classnames libraries.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,44 @@
 (function() {
     'use strict';
 
+    const _styled = styled.default;
+
+    const StyledItem = _styled.div`
+      width: 64px;
+      height: 64px;
+      filter: contrast(${props => props.active ? 100 : 80}%)
+              brightness(${props => props.active ? 100 : 30}%);
+    `;
+    
+    const Item = (props) =>
+      <StyledItem
+        className={classNames(props.name, props.value && `${props.name}--active`)}
+        active={props.value}
+        onClick={() => props.onClick(props.name)} />;
+
+    class App extends React.Component {
+        state = { items: items() }
+        
+        render() {
+            const items = this.state.items;
+            return <Item
+              name="bow"
+              value={items.bow}
+              onClick={this.toggle} />;
+        }
+
+        toggle = (name) => {
+            const items = this.state.items;
+            this.setState({ items: { ...items, [name]: !items[name] } });
+        }
+    }
+
+    const items = () => {
+        return {
+            bow: false
+        };
+    };
+
     window.start = function() {
         ReactDOM.render(<App/>, document.getElementById('app'));
     };
