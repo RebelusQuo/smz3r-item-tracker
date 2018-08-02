b6c84a9d2a058b68afe6f7e7c7141b57aecbbfbd
Roger Norling <roger.norling.se@gmail.com>
2018-08-02 14:59:35 +0200

    Show an item with multiple levels

--- src/main.jsx
+++ src/main.jsx
@@ -16,26 +16,51 @@
         active={props.value}
         onClick={() => props.onClick(props.name)} />;
 
+    const LeveledItem = (props) =>
+      <StyledItem
+        className={classNames(props.name, props.value && `${props.name}--active-${props.value}`)}
+        active={props.value}
+        onClick={() => props.onClick({ raise: props.name })}
+        onContextMenu={(e) => { props.onClick({ lower: props.name }); e.preventDefault(); }} />
+
     class App extends React.Component {
         state = { items: items() }
         
         render() {
             const items = this.state.items;
-            return <Item
-              name="bow"
-              value={items.bow}
-              onClick={this.toggle} />;
+            return <React.Fragment>
+              <Item
+                name="bow"
+                value={items.bow}
+                onClick={this.toggle} />
+              <LeveledItem
+                name="glove"
+                value={items.glove}
+                onClick={this.level} />
+            </React.Fragment>;
         }
 
         toggle = (name) => {
             const items = this.state.items;
             this.setState({ items: { ...items, [name]: !items[name] } });
         }
+
+        level = ({ raise, lower }) => {
+            const name = raise || lower;
+            const delta = raise ? 1 : -1;
+            const items = this.state.items;
+            const modulo = 1 + items.limit[name];
+            this.setState({ items: { ...items, [name]: (items[name] + modulo + delta) % modulo } });
+        }
     }
 
     const items = () => {
         return {
-            bow: false
+            bow: false,
+            glove: 0,
+            limit: {
+              glove: 2
+            }
         };
     };
 
